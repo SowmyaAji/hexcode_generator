171210ebf887a7e987d9e3ba43827c9872015746
Sowmya Aji <sowmya.aji@gmail.com>
2022-08-22 22:58:28 -0400

    Add final test

--- tests.py
+++ tests.py
@@ -36,6 +36,13 @@ class TestHexCodes(unittest.TestCase):
             results = [("831e33be1b6f6bfe7356664a9cf088b3722371f1e38bc575781b216db2508b00",)]
             res = validate_not_used(conn, "1DB06C53", "831e33be1b6f6bfe7356664a9cf088b3722371f1e38bc575781b216db2508b00", results, conn.cursor(), 3)
             self.assertEqual(res, "Hex code already used earlier", f"Hashcode not in db")
+            
+    def test_validate_not_used_drop(self):
+        with patch('db.sqlite3') as conn:
+            results = [("7b4379d483c3059191bb1370e059bd20a2be60b8c64d9e26cc942322a7d849cf",),("3106bed73654e22e91408787b28c1a5dd6d8fad82db74d6d5257c3425e8949b0", ),
+                        ("f97b17e884efcb66c97022ce119259350ab806ec51d1e263d118252536c2e44a",)]
+            res = validate_not_used(conn, "1DB06C53", "831e33be1b6f6bfe7356664a9cf088b3722371f1e38bc575781b216db2508b00", results, conn.cursor(), 3)
+            self.assertEqual(res, "All valid hexcodes exhausted, restarting ...", f"All unique codes are not done")
         
 
 if __name__ == '__main__':
